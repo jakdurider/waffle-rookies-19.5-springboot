ee5ed2c2ae7981b59a437ab67b95b9f443cfc8c3
jakdurider <970707zzang@naver.com>
2021-10-18 18:29:34 +0900

    add results

--- seminar/src/main/kotlin/com/wafflestudio/seminar/domain/ping/api/PingController.kt
+++ seminar/src/main/kotlin/com/wafflestudio/seminar/domain/ping/api/PingController.kt
@@ -2,12 +2,14 @@ package com.wafflestudio.seminar.domain.ping.api
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 @RestController
+@RequestMapping("/")
 class PingController {
-    @GetMapping("/")
+    @GetMapping("/ping/")
     fun ping(): ResponseEntity<String>{
         return ResponseEntity.ok("pong");
     }
